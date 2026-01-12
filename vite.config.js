bb93e6339a16b33b8a0ab44f39294703bf3f90e4
akeem-steno <73497420+akeem-steno@users.noreply.github.com>
2026-01-12 11:48:35 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// base: './' makes the build work on GitHub Pages (including repo subpaths).
 export default defineConfig({
   plugins: [react()],
-  base: './'
+  base: '/depoardy/',
 })
